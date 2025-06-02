27472e94851955ec4679c1f5e28ac05570647712
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-06-02 10:55:36 +0700

    membuat baseClass public

--- noPewaris/noPewaris.cpp
+++ noPewaris/noPewaris.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+class baseClass {
+    public:
+        virtual void perkenalan()final{
+            cout << "hallo saya function dari base class";
+        }
+};
