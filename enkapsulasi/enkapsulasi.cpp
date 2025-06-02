7a9abe71561db4279298e45faa8889e80212a95e
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-06-02 10:20:57 +0700

    menjalankan remote lampu rumah

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -12,3 +12,19 @@ class remotelampu {
         return saklarNo[i];
     }
 };
+
+int main(){
+    remotelampu lampuRumah;
+
+    lampuRumah.setsaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setsaklarNo(1, "Lampu Ruamg Tamu");
+    lampuRumah.setsaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setsaklarNo(3, "Lampu Teras Rumah");
+
+    cout << lampuRumah.getsaklarNo(0) <<endl;
+    cout << lampuRumah.getsaklarNo(1) <<endl;
+    cout << lampuRumah.getsaklarNo(2) <<endl;
+    cout << lampuRumah.getsaklarNo(3) <<endl;
+
+    return 0;
+}
