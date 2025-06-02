bfcc114dc4740361b3dc8c317f48de2c398ea94f
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-06-02 10:00:32 +0700

    metode mengisi nilai private member dan public

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
 using namespace std;
+
+class AbstrakiKlas {
+    private: string x, y;
+
+    public:
+
+    void setXY(string a, string b,){
+        x = a;
+        y = b;
+    }
+}
