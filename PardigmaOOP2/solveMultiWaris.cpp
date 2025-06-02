c7e0b74206217e9ab1d9bcb22a0b972b365bdc45
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-06-02 11:08:14 +0700

    menjalankan progres

--- PardigmaOOP2/solveMultiWaris.cpp
+++ PardigmaOOP2/solveMultiWaris.cpp
@@ -31,3 +31,19 @@ class pelajar : public orang {
             }
         };
 
+class budi : public pekerja, public pelajar {
+    public:
+
+        budi(int pUmur):
+            pekerja(pUmur),
+            pelajar(pUmur)
+            {
+                cout << "budi dibuat"<< "\n" << endl;
+            }
+};
+
+int main(){
+    budi a(12);
+
+    return 0;
+}
